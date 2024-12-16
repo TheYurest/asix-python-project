59ceaf84383f4c5bfe6cf7bf48f76b02ebfed41b
Yurest <yurest06@gmail.com>
2024-12-16 15:48:57 +0100

    exits instead of elses on modules

--- var_globals.py
+++ var_globals.py
@@ -1,5 +1,6 @@
 # var_globals.py: Variables globals, com l’amplada del mapa o l’energia inicial.
+# moviments.py: Implementa les regles per moure l’explorador.
 if __name__=="__main__":
     print("ERROR: s'ha d'executar l'arxiu \"ppal.py\"")
-else:
-    pass # CODI AQUI
+    exit()
+#CODI AQUÍ
